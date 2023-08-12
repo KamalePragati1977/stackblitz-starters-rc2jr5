34e9b37167705cf42666be8b537e0420c1b27cfc
KamalePragati1977 <141109450+KamalePragati1977@users.noreply.github.com>
2023-08-12 13:37:20 +0600

    1 file modified, 3 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,20 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroDetailsComponent } from '../hero-details/hero-details.component';
 import { HEROES } from '../mock-heroes';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   standalone:true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
+  imports:[HeroComponent]
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[]= HEROES;
   constructor() { 
-    console.log(this.heroes);
+    // console.log(this.heroes);
   }
 
   ngOnInit() {
+    this.heroes=HEROES;
+    console.log(this.heroes);
   }
 
 }
